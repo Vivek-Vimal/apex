7dd6ada4fcb4bedba86118a74e3ea8e2f01aec4c
vivek-vimal <vimalvivek50@gmail.com>
2022-12-20 01:53:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+import LandingPage from './LandingPage'
+//import Referal from './Page/referral'
+import { Routes, Route } from 'react-router-dom'
+//import Lending from './Page/lending'
+
+const App = () => {
+
+  return (
+    <>
+      <Navbar /> 
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        {/* <Route path='/fund' element={<div />} /> */}
+        {/* <Route path='/whyUs' element={<Referal />} /> */}
+        {/* <Route path='/media' element={ <Lending /> } /> */}
+      </Routes>
+    </>
+  )
+}
+
+export default App
